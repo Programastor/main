2e90ae1718981c06a6da48b837abc9c38ef31354
Aleksey <borisov_ai@bk.ru>
2026-09-25 11:05:24 +0300

    урок 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
 void main() {
-    System.out.println("привет Астон!");
-    for (int i = 1; i <= 1; i++) {
-        System.out.println("Привет Глеб");
-    }
+    System.out.println("привет второй урок");
 }
